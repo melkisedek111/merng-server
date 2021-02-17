10ee66af222fe478688f97e1e1b0016545204d83
Melkisedek Ubalde <melkisedek222@gmail.com>
2021-02-17 14:00:50 +0800

    init

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://classed:OWt3xoALkSWaC16X@cluster0.0re0d.mongodb.net/socialmediadb?retryWrites=true&w=majority',
+    SECRET_KEY: '81CD45790D722FEB9B3E9E94A696C15B'
+}
